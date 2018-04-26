4fa60a6b5595f9296d4947a74c655fc92a0693fe
Rob <robmclean.personal@gmail.com>
2018-04-25 20:42:35 -0600

    testing different body parser layouts

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ var {Inspection} = require('./models/inspection');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json(['application/json']));
-app.use(bodyParser.urlencoded([{extended: false}, urlencoded]));
+app.use(bodyParser.json('application/json'));
+app.use(bodyParser.urlencoded({extended: false}, 'application/x-www-form-urlencoded'));
 
 app.use(express.static(__dirname + '/public'));
 
