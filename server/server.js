0b11154d6107ddd15f58d0faa21701e8b1e6c85d
Rob <robmclean.personal@gmail.com>
2018-04-26 13:44:27 -0600

    Setup Test and dev ENVs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -13,7 +14,7 @@ var {User} = require('./models/user');
 var {Inspection} = require('./models/inspection');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json('application/json'));
 app.use(bodyParser.urlencoded({extended: false}, 'application/x-www-form-urlencoded'));
