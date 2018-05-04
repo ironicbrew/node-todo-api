eebeed2ac1ccb59bff74ac4f4912b1b4edbc65c5
Rob <robmclean.personal@gmail.com>
2018-05-04 14:26:48 -0600

    added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json('application/json'));
-app.use(bodyParser.urlencoded({extended: false}, 'application/x-www-form-urlencoded'));
+// app.use(bodyParser.urlencoded({extended: false}, 'application/x-www-form-urlencoded'));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -167,6 +167,18 @@ app.get('/dashboard', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then((user) => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
 
 module.exports = {app};
 
